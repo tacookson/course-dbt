5c958cce98d42dbf792cae707d4088269c225c14
Alex Cookson <alexander.cookson@gmail.com>
2024-04-02 14:06:49 +0000

    Add testing to int and mart models

--- greenery/models/marts/core/intermediate/int_user_order_metrics.sql
+++ greenery/models/marts/core/intermediate/int_user_order_metrics.sql
@@ -10,11 +10,19 @@ final as (
 
     select
 
+        -- primary key
         user_id,
+
+        -- foreign keys
+        -- none
+
+        -- properties
         count(distinct order_id) as order_count,
         /* only include order_cost as part of spend calculation, since shipping cost is a direct offest */
         round(avg(order_cost), 2) as aov,
         sum(order_cost) as lifetime_spend,
+
+        -- timestamps
         min(created_at) as first_order_at,
         max(created_at) as last_order_at
 
